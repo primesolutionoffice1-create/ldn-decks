d90339ac0b5295fc1ec7907cd2ed76cbeaa78d14
primesolutionoffice1-create <primesolution.office1@gmail.com>
2026-05-06 20:08:55 -0400

    fix(seo): add LocalBusiness address/telephone/url to Service schema (Google rich results)

--- src/components/ServiceSchema.jsx
+++ src/components/ServiceSchema.jsx
@@ -14,7 +14,7 @@ export default function ServiceSchema({ name, description, price, areaServed }) 
     provider: {
       '@type': 'LocalBusiness',
       '@id': 'https://ldndecks.com/#organization',
-      name: 'Loudoun Decks',
+      name: 'Loudoun Decks', url: 'https://ldndecks.com', telephone: '+1-571-655-7207', address: { '@type': 'PostalAddress', streetAddress: '13704 Winding Oak Cir', addressLocality: 'Centreville', addressRegion: 'VA', postalCode: '20121', addressCountry: 'US' },
     },
     areaServed: areaServed || [
       { '@type': 'AdministrativeArea', name: 'Loudoun County, VA' },
